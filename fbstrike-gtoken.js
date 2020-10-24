250b1122220455481146e144f713146213a75222
VerviumStrike <verviumstrike@protonmail.com>
2020-10-24 16:06:19 -0700

    FBStrike-GToken

---
+++ fbstrike-gtoken.js
@@ -0,0 +1,48 @@
+//Requirements Importer
+let { PythonShell } = require("python-shell")
+const J2Url = require("j2url")
+const Express = require("express")
+const Chalk = require("chalk")
+
+//Variables
+const Port = process.env.PORT || 8176
+const Web = Express()
+const Args = process.argv.slice(2)
+
+//Express
+Web.get("/", function(req, res){
+    var username = J2Url.getParam(req.originalUrl, "user")
+    var password = J2Url.getParam(req.originalUrl, "pass")
+
+    var options = {
+        args: ['--username', username, '--password', password]
+    }
+    PythonShell.run("./resources/index.py", options, function(err, stdout){
+        if(err){
+            console.log(Chalk.red("[FBStrike-GToken] Invalid username or password."))
+            return
+        }
+        try{
+            stdout = stdout.toString()
+            if(stdout.indexOf("Error") != -1){
+                res.json({
+                    "error": "Invalid username or password."
+                })
+            }else{
+                res.json({
+                    "success": stdout
+                })
+            }
+        }catch{
+            res.json({
+                "error": "Invalid username or password."
+            })
+        }
+    })
+})
+Web.listen(Port, ()=>{
+console.log(`=============================
+    Server Status: Online
+    Port: ${Port}
+=============================`)
+})
---
+++ resources/index.py
@@ -0,0 +1,26 @@
+import hashlib, requests, argparse, json
+
+def GToken(data):
+    r = requests.get('https://api.facebook.com/restserver.php',params=data)
+    a = json.loads(r.text)
+    print(a['access_token'])
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--username")
+parser.add_argument("--password")
+args = parser.parse_args()
+
+username = args.username
+password = args.password
+
+username = username.replace("-", " ")
+
+try:
+    API_SECRET = '62f8ce9f74b12f84c123cc23437a4a32';data = {"api_key":"882a8490361da98702bf97a021ddc14d","credentials_type":"password","email":username,"format":"JSON", "generate_machine_id":"1","generate_session_cookies":"1","locale":"en_US","method":"auth.login","password":password,"return_ssl_resources":"0","v":"1.0"};sig = 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+username+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+password+'return_ssl_resources=0v=1.0'+API_SECRET
+    x = hashlib.new('md5')
+    x.update(sig)
+
+    data.update({'sig':x.hexdigest()})
+    GToken(data)
+except:
+    print("Error: invalid username or password.")
